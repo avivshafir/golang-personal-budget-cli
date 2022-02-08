b4834dc8d5c341ad8474092f11977ec163cf64c8
Aviv Shafir <aviv.shafir@lusha.com>
2022-02-08 19:30:17 +0200

    structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,17 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	// Amount stores the budget amount
+	Max float64
+	// Currency stores the budget currency
+	Items []Item
+}
 
 // Item stores item information
+type Item struct {
+	// Name stores the item name
+	Description string
+	// Amount stores the item amount
+	Price float64
+}
