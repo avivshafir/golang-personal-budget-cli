5cbdb9d488680c562382ed74f41d5d3a1aca91bf
Aviv Shafir <aviv.shafir@lusha.com>
2022-02-08 20:06:02 +0200

    structs

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -77,11 +77,11 @@ func (b *Budget) RemoveItem(description string) {
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
 
-	if _, hasEntry := report[month]; hasEntry {
-		return nil, errDuplicateEntry
-	}
 	if len(report) >= 12 {
 		return nil, errReportIsFull
+	}
+	if _, hasEntry := report[month]; hasEntry {
+		return nil, errDuplicateEntry
 	}
 
 	newBudget = &Budget{Max: max}
